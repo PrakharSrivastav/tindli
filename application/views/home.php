b0a9ccc0d421a37bd00b404bd969fb2931721fc2
prakharsrivastav <srivprakhar@gmail.com>
2015-11-18 11:44:49 +0530

    Prakhar : Added basic code template with framework to repo

---
+++ application/views/home.php
@@ -0,0 +1,47 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!-- above statement is required for security purpose so that none can call this page directly. -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Metadata description for this page -->
+  <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+  <!-- Title -->
+  <title>Tindli</title>
+
+  <!-- Include required stylesheets below -->
+	<!-- Bootstrap default css-->
+	<link href="<?=base_url()?>css/bootstrap.min.css" rel="stylesheet">
+  <!-- User defined css -->
+	<link href="<?=base_url()?>css/style.css" rel="stylesheet">
+  
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--
+      [if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]
+    -->
+  </head>
+  <body>
+  	
+    <!-- bootstrap container -->
+    <div class="container">
+  		<h1 class="text-center"> Hello, Angular!" </h1>
+  	</div>
+    
+    <!-- include required javascript libraries below -->
+    <!-- JQuery -->
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  	<!-- Bootstrap -->
+    <script src="<?=base_url()?>js/bootstrap.min.js"></script>
+  	<!-- AngularJS -->
+    <script src="<?=base_url()?>js/angular.min.js"></script>
+  	<!-- application specific js file -->
+    <script src="<?=base_url()?>js/app.js"></script>
+  
+  </body>
+  </html>
